15fd0281fa300647e71981be5079cca0016708b6
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-05-02 12:44:10 +0900

    [jkan] mybatis camelCase 변환 파일 및 소스 추가

--- HOST_WEB/src/main/java/com/daon/host/web/vo/Notice.java
+++ HOST_WEB/src/main/java/com/daon/host/web/vo/Notice.java
@@ -9,25 +9,14 @@ import lombok.Setter;
 @Setter
 public class Notice {
 
-	private String notice_no; // 공지번호
-	private String apt_id; // 단지코드
-	private String notice_writer; // 공지작성자
-	private String notice_cont; // 공지내용
-	private String notice_title; // 공지제목
-	private String remk; // 비고
-	private String ins_id; // 입력자
-	private Date ins_date; // 입력시간
-	private String upd_id; // 수정자
-	private Date upd_date; // 수정시간
-
-//	private String noticeNo; // 공지번호
-//	private String aptId; // 단지코드
-//	private String noticeWriter; // 공지작성자
-//	private String noticeContent; // 공지내용
-//	private String noticeTitle; // 공지제목
-//	private String remark; // 비고
-//	private String insId; // 입력자
-//	private Date insDate; // 입력시간
-//	private String updId; // 수정자
-//	private Date updDate; // 수정시간
+	private String noticeNo; // 공지번호
+	private String aptId; // 단지코드
+	private String noticeWriter; // 공지작성자
+	private String noticeContent; // 공지내용
+	private String noticeTitle; // 공지제목
+	private String remark; // 비고
+	private String insId; // 입력자
+	private Date insDate; // 입력시간
+	private String updId; // 수정자
+	private Date updDate; // 수정시간
 }
