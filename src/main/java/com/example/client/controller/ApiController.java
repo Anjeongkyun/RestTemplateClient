0e1ca1b4b98db813d4b4de68ea8bca2da7fba704
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-06-09 11:17:51 +0900

    post방식 구현

--- src/main/java/com/example/client/controller/ApiController.java
+++ src/main/java/com/example/client/controller/ApiController.java
@@ -20,6 +20,6 @@ public class ApiController {
     @GetMapping("/hello")
     public UserResponse getHello() {
 
-        return restTemplateService.hello();
+        return restTemplateService.post();
     }
 }
--- src/main/java/com/example/client/service/RestTemplateService.java
+++ src/main/java/com/example/client/service/RestTemplateService.java
@@ -1,11 +1,13 @@
 package com.example.client.service;
 
+import dto.UserRequest;
 import dto.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.xml.ws.Response;
 import java.net.URI;
 
 @Service
@@ -31,5 +33,31 @@ public class RestTemplateService {
         System.out.println(result.getBody());
 
         return result.getBody();
+    }
+
+    public UserResponse post() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:5959")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand("60", "jeongkyun")
+                .toUri();
+
+        System.out.println(uri);
+
+        // http body -> object -> object mapper -> json -> rest template -> http boddy json
+        UserRequest req = new UserRequest();
+        req.setName("jeongkyun");
+        req.setAge(66);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> res = restTemplate.postForEntity(uri, req, UserResponse.class);
+
+        System.out.println(res.getStatusCode());
+        System.out.println(res.getHeaders());
+        System.out.println(res.getBody());
+
+        return res.getBody();
     }
 }
